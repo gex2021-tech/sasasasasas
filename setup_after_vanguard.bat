25e9e6737677a38e56aac9e9f5ba169efe6884e1
Nicolas Flores <gex20@DESKTOP-L90GD1K.1008810693-39812694-317082953>
2026-08-04 20:23:56 -0400

    fix(script): force current working directory to script location (%~dp0)

--- setup_after_vanguard.bat
+++ setup_after_vanguard.bat
@@ -1,5 +1,6 @@
 @echo off
 setlocal enabledelayedexpansion
+cd /d "%~dp0"
 :: ============================================
 :: VGC Emulator - Setup After Vanguard Install
 :: ============================================
